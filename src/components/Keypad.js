79e0169eedf5b0d486c15c14b2faf9a05a89ab5e
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-12-21 15:07:53 -0500

    commit tree 45e5d32f16b68bf789ba44945faca8a386294c09

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,30 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("I submit")
+    }
+
+    function handleChange(event){
+        // console.log(`${event.target.name}: ${event.target.value}`);
+        console.log('Entering password...');
+    }
+  
+    return(
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="password" 
+            name="password" 
+            onChange={handleChange}
+            placeholder="Enterting password..." 
+            />
+            <button>Submit</button>
+        </form>
+    </div> 
+    );  
+}
+
+export default Keypad;
